bd18af370818f66c21e03f63d4e5f657afe5e3a7
MdFazilGITHUB <mmilyas540@gmail.com>
2025-08-29 15:01:52 +0530

    fixed vector db chunking error in embeddings

--- rag.js
+++ rag.js
@@ -3,36 +3,43 @@ import { PineconeStore } from "@langchain/pinecone";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
+// 2. Setup Gemini embeddings
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  apiKey: process.env.GEMINI_API_KEY,
+  model: "text-embedding-004"
+});
+
 export async function buildVectorStore(docs) {
-    // 1. Split documents
+  // 1. Split documents
   const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 80,
-    chunkOverlap: 10,
+    chunkSize: 200,
+    chunkOverlap: 100
   });
   const splitDocs = await splitter.splitDocuments([docs]);
 
-
-  // 2. Setup Gemini embeddings
-  const embeddings = new GoogleGenerativeAIEmbeddings({
-    apiKey: process.env.GEMINI_API_KEY,
-    model: "text-embedding-004",
-  });
-
   // 3. Connect to Pinecone
   const pinecone = new Pinecone({
-    apiKey: process.env.PINECONE_API_KEY,
+    apiKey: process.env.PINECONE_API_KEY
   });
 
   const index = pinecone.Index(process.env.PINECONE_INDEX);
+  const namespace =index.namespace("pdfs");
 
   // 4. Store docs in Pinecone
-  const vectorStore = await PineconeStore.fromDocuments(splitDocs, embeddings, {
+  await PineconeStore.fromDocuments(splitDocs, embeddings, {
     pineconeIndex: index,
+    namespace: "pdfs"
   });
 
-  return vectorStore;
+  return namespace;
 }
 
-export async function queryVectorStore(store, query) {
-  return await store.similaritySearch(query, 3); // top 3 matches
+export async function queryVectorStore(store, query, path) {
+  const vector = await embeddings.embedQuery(query);
+  return await store.query({
+    topK: 100,
+    vector,
+    filter: { source: path },
+    includeMetadata: true
+  });
 }
